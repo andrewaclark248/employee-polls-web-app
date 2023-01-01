f88a92eb0ffedd1959b33fcdd045f09675fd248b
andrewaclark248 <andrewaclark248@gmail.com>
2022-12-31 23:21:12 -0500

    add form

--- src/NewPoll.js
+++ src/NewPoll.js
@@ -8,14 +8,30 @@ function NewPoll(props) {
     return (
     <div >
         <h1 className="bottom-padding">New Poll</h1>
-        <div className="pb-3">
-            <span>This is a new Poll</span>
-            <span>This is a new Poll</span>
-            <span>This is a new Poll</span>
-            <span>This is a new Poll</span>
-            <span>This is a new Poll</span>
-            <span>This is a new Poll</span>
-            <span>This is a new Poll</span>
+
+        <div className="row">
+            <div className="col-3"></div>
+            <div className="col-6">
+                <div className="card" >
+                    <div className="card-header text-white bg-primary">New Poll</div>
+                    <div className="card-body">
+                        <h5 className="card-title pb-4">Would You Rather?</h5>
+                        <div className="pb-3">
+                            <label className="">First Option</label>
+                            <input className="form-control" placeholder="First Option"/>
+                        </div>
+                        <div className="pb-5">
+                            <label className="">Second Option</label>
+                            <input className="form-control" placeholder="Second Option"/>
+                        </div>
+                        <div className="pb-2">
+                            <button className="btn btn-primary" onClick={() => { props.dispatch({type: "sdfd" })}}>Create Poll</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-3"></div>
+
         </div>
       </div>
     );
