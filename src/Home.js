e73ee0156e1f7162cefc2d65f1cdc377860ff43a
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-01 12:58:32 -0500

    add map func

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,11 @@ function Home(props) {
   var userUnansweredPolls = null;
     if (props.allPolls.length > 0) {
       userUnansweredPolls = unansweredPolls(props.allPolls, props.currentUser)
-      console.log(userUnansweredPolls)
+      console.log(userUnansweredPolls != null)
     }
     
     return (
-      <div >
+      <div>
           <div className="bottom-padding">
             <h1>Home Page</h1>
             <span>Current User: {props.currentUser}</span>
@@ -21,10 +21,21 @@ function Home(props) {
               <div className="card" >
                 <div className="card-header text-white bg-primary">Unawnsered Polls</div>
                 <div className="card-body">
+
                   <div className="">
+                    {userUnansweredPolls != null &&
+                      userUnansweredPolls.map((poll, index) => {
+                        return (<div className="row" key={index}>
+                          <div className="col-3">
+                            <span className="text-dark">somse data ahahahahha</span>
+                          </div>
 
-
+                        </div>)
+                      })
+                    }
                   </div>
+
+
                 </div>
               </div>
             </div>
