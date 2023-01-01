f2b329387ddeb2545c88b1be0a6d5c8ae87346f6
andrewaclark248 <andrewaclark248@gmail.com>
2022-12-31 20:28:53 -0500

    add polls

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,34 @@ function Home(props) {
 
     return (
       <div >
-          <h1>Home Page</h1>
-          <span>Current User: {props.currentUser}</span>
+          <div className="bottom-padding">
+            <h1>Home Page</h1>
+            <span>Current User: {props.currentUser}</span>
+          </div>
+
+          <div className="row">
+            <div className="col-2"></div>
+
+            <div className="col-4">
+              <div className="card" >
+                <div className="card-header text-white bg-primary">Unawnsered Polls</div>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4">
+              <div className="card" >
+                <div className="card-header text-white bg-primary">Awnsered Polls</div>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-2"></div>
+          </div>
       </div>
     );
   }
