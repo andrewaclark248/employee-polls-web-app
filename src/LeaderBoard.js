fc992f71a3206dc5129e3bcdef2485d77fde136f
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-15 17:04:52 -0500

    add page

---
+++ src/LeaderBoard.js
@@ -0,0 +1,54 @@
+
+
+export default function LeaderBoard(props) {
+
+    const allUsers = ['john-doe', 'jane-doe', 'batman-robin']
+
+    return (
+        <div>
+            <div className="row">
+                <center>
+                    <h2>Leader Board</h2>
+                </center>
+            </div>
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-8">
+
+                    <div className="card" >
+                        <div className="card-header text-white bg-primary">Leader Board</div>
+                        <div className="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                    <th>UserName</th>
+                                    <th>Avatar</th>
+                                    <th># of Questions Asked</th>
+                                    <th># of Questions Anwsered</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="col-2"></div>
+            </div>
+        </div>
+    );
+
+}
